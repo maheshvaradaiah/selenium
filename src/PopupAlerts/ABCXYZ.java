0bba6f7bf9619257a532a97cb74c058457792b79
maheshvaradaiah <maheshvaradaiah@gmail.com>
2025-05-14 12:09:11 +0530

    New ABCXYZ class added in alert package

---
+++ src/PopupAlerts/ABCXYZ.java
@@ -0,0 +1,14 @@
+package PopupAlerts;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class ABCXYZ {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
